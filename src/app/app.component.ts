b57d41ac79a7e9733dd5b76fb9d6f25158e97d61
57130500041 <zoyoletfame@hotmail.com>
2017-10-18 20:33:29 +0700

    fame

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
-
+import { equalizerPage } from '../pages/equalizer/equalizer';
 @Component({
   templateUrl: 'app.html'
 })
@@ -22,7 +22,8 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'List', component: ListPage },
+      { title: 'Equalizer', component: equalizerPage }
     ];
 
   }
---
+++ src/pages/equalizer/equalizer.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-equalizer',
+  templateUrl: 'equalizer.html'
+})
+export class equalizerPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
