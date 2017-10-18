1b4a4d1fcdac9a07820c72650c850c5b5cbd9d1d
mix11503 <mix11503@gmail.com>
2017-10-18 23:25:58 +0700

    save to merge

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, PopoverController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -9,13 +9,19 @@ export class HomePage {
 
   tracks: Track[] = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private popoverCtrl: PopoverController) {
     this.tracks = [
                   {name: "song1", artis: "artis1", time: "3:40"},
                   {name: "song2", artis: "artis2", time: "3:40"},
                   {name: "song3", artis: "artis3", time: "3:40"},
                   {name: "song4", artis: "artis4", time: "3:40"}
                 ];
+  }
+  presentSort(myEvent) {
+    let popover = this.popoverCtrl.create(PopoverPage);
+    popover.present({
+      ev: myEvent
+    });
   }
 }
 
